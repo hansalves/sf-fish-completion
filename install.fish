9ed6d016f1e03b6a1caa42c5919528ef25101a1d
Hans Alves <halves@qzwx.nl>
2025-02-18 21:11:58 +0100

    comment install.fish

--- install.fish
+++ install.fish
@@ -1,4 +1,10 @@
 #!/usr/bin/env fish
+
+# Generate the bash completion code
 sf autocomplete script bash
+
+# Create the fish completions directory if it doesn't exist
 mkdir -p ~/.config/fish/completions/
+
+# Generate the fish completion file
 ./generate-completion.py (bash -c 'eval $(sf autocomplete script bash); echo $SF_AC_BASH_COMPFUNC_PATH') > ~/.config/fish/completions/sf.fish
